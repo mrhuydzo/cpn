6501766ce7bbac43caaf884964baf507d0e722f5
huynq1 <huydzo88@gmail.com>
2021-06-01 18:21:13 +0700

    - update

--- build/js/main.js
+++ build/js/main.js
@@ -5,7 +5,23 @@ function backToTop () {
     });
 }
 
+function slideNews () {
+    var swiper = new Swiper(".news_slide .swiper-container", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
+
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
     backToTop();
+    slideNews ();
 })
