53b247f1f693e0ed888d65d0588f07ab3c78f451
huynq1 <huydzo88@gmail.com>
2021-06-01 13:14:42 +0700

    - update

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,3 @@
 $(document).ready(function () {
-    var swiper = new Swiper('.banner_home .swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+    $('[data-toggle="tooltip"]').tooltip()
 })
