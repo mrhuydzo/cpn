21daeb91d475c41e4a14d0e2e35d2136b60cb52e
huynq1 <huydzo88@gmail.com>
2021-06-01 13:28:26 +0700

    - update

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,11 @@
+function backToTop () {
+    $("a.back_to_top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "fast");
+        return false;
+    });
+}
+
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
+    backToTop();
 })
