e88af4ac6073b86c164371bf257b480118d56c60
huynq1 <huydzo88@gmail.com>
2021-06-02 11:23:03 +0700

    - update

--- build/js/main.js
+++ build/js/main.js
@@ -6,16 +6,27 @@ function backToTop () {
 }
 
 function slideNews () {
-    var swiper = new Swiper(".news_slide", {
+    var swiper = new Swiper(".block_news .news_slide", {
         slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
+        spaceBetween: 15,
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".block_news .block_header__control .next",
+            prevEl: ".block_news .block_header__control .prev",
+        },
+    });
+}
+
+function slideCustomer () {
+    var swiper = new Swiper(".block_customer .customer_slide", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        navigation: {
+            nextEl: ".block_customer .block_header__control .next",
+            prevEl: ".block_customer .block_header__control .prev",
+        },
+        pagination: {
+            el: ".block_customer .swiper-pagination",
+            clickable: true,
         },
     });
 }
@@ -24,4 +35,5 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
     backToTop();
     slideNews ();
+    slideCustomer ();
 })
